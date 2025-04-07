757b1715d56d72fc936bcdafb29cdd1446198dc8
melty-glass <109243017+melty-glass@users.noreply.github.com>
2025-04-07 19:20:28 +0700

    Update script.js
    added comments + a one-liner version cause why not

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-ans = document.querySelectorAll('.rmq-3ca56ea3');
+// long version
+ans = document.querySelectorAll('.rmq-3ca56ea3'); // select all answers on the page
 for (i of ans) {
-    i.children[2].children[0].style.filter = "";
-    i.children[2].children[1] ? i.children[2].children[1].remove() : null;
+    i.children[2].children[0].style.filter = ""; // remove the blur filter from the inline css
+    i.children[2].children[1] ? i.children[2].children[1].remove() : null; // if it exists, remove it; else, do nothing
 }
+
+// one-liner version
+a=document.querySelectorAll('.rmq-3ca56ea3');for(i of a){i.children[2].children[0].style.filter="";i.children[2].children[1]?i.children[2].children[1].remove():null}
