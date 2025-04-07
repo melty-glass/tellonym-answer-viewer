2fd64d2d09868e40964bc8d32fea70dc1babe3d7
melty-glass <109243017+melty-glass@users.noreply.github.com>
2025-04-07 19:54:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // long version
 ans = document.querySelectorAll('.rmq-3ca56ea3'); // select all answers on the page
+ans[ans.length-1].remove(); // removes the last element in the list, which blocks the last answer on the page. don't ask why it's structured like this.. i don't know
 for (i of ans) {
     i.children[2].children[0].style.filter = ""; // remove the blur filter from the inline css
     i.children[2].children[1] ? i.children[2].children[1].remove() : null; // if it exists, remove it; else, do nothing
 }
 
-// one-liner version
-a=document.querySelectorAll('.rmq-3ca56ea3');for(i of a){i.children[2].children[0].style.filter="";i.children[2].children[1]?i.children[2].children[1].remove():null}
+// one-liner version cause why not
+    a=document.querySelectorAll('.rmq-3ca56ea3');a[a.length-1].remove();for(i of a){i.children[2].children[0].style.filter="";i.children[2].children[1]?i.children[2].children[1].remove():null}
